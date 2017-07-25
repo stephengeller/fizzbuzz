228b6b17b7e20cdab776c41d5b8a0d2c014f945f
stephengeller <stephen.geller@hotmail.com>
2017-07-25 11:58:58 +0100

    Integrated fizzbuzz method to Fixnum class to suit .fizzbuzz calling on rspec

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,10 @@
-def fizzbuzz(num)
-  return "fizzbuzz" if num%15 == 0
-  return "fizz" if num%3 == 0
-  return "buzz" if num%5 == 0
-  return num.to_s
+class Fixnum
+
+  def fizzbuzz
+    return "fizzbuzz" if self%15 == 0
+    return "fizz" if self%3 == 0
+    return "buzz" if self%5 == 0
+    return self.to_s
+  end
+
 end
